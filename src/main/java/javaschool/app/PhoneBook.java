5ba72380c86585926f3a7b3fce387cbff14d7044
Aleksandra <fgh>
2017-03-29 20:50:45 +0300

    record -> person+ name

--- src/main/java/javaschool/app/PhoneBook.java
+++ src/main/java/javaschool/app/PhoneBook.java
@@ -9,15 +9,23 @@ public class PhoneBook {
     private List<Record> recordList = new ArrayList<>();
 
     @Command
-    public void create(String name, String address, String age, String... phone) {
-        Record r = new Record();
+    public void createPerson(String name, String address, String age, String... phone) {
+        Person r = new Person();
         r.setName(name);
         r.setAddress(address);
         r.setAge(age);
         r.addPhones(phone);
-
         recordList.add(r);
     }
+
+    @Command
+    public void createNote(String name, String note) {
+        Note r = new Note();
+        r.setName(name);
+        r.setNote(note);
+        recordList.add(r);
+    }
+
     @Command
     public List<Record> list() {
         return recordList;
@@ -26,8 +34,9 @@ public class PhoneBook {
     @Command
     public void addPhone(int id, String phone) {
         for (Record r : recordList) {
-            if (r.getId() == id) {
-                r.addPhones(phone);
+            if (r instanceof Person && r.getId() == id) {
+                Person p = (Person) r;
+                p.addPhones(phone);
                 break;
             }
         }
@@ -39,9 +48,20 @@ public class PhoneBook {
         List<Record> result = new ArrayList<>();
         for (Record r : recordList) {
             String name = r.getName().toLowerCase();
-            if (name.contains(str)) {
-                result.add(r);
+            String address;
+            if (r instanceof Person) {
+                Person p = (Person) r;
+                address = p.getAddress().toLowerCase();
+            } else {
+                address = "";
             }
+
+//            List<String> phones = r.getPhone();
+//            for (String phone : phones){
+//            if (name.contains(str) ||address.contains(str)||phone.toLowerCase().contains(str) ) {
+//                result.add(r);
+//            }
+//            }
         }
 
         return result;
